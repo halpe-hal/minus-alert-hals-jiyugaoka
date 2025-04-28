2a0fbc67ff0aefb6636d0521dcafaf22a376cd8f
halpe-hal <nishimura@kklia.com>
2025-04-28 16:11:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ import os
 
 app = FastAPI()
 
-# Streamlitアプリ起動
-def start_streamlit():
-    port = os.environ.get("PORT", "10000")  # Render用に環境変数PORTを拾う
-    subprocess.Popen([
-        "streamlit", "run", "streamlit_app.py",
-        "--server.port", port,
-        "--server.headless", "true",
-        "--server.address", "0.0.0.0"  # これも必須！！
-    ])
-
-# サーバー起動時にStreamlitも一緒に起動する
-threading.Thread(target=start_streamlit, daemon=True).start()
-
 # /run_notify にアクセスされたらLINE通知する
 @app.get("/run_notify")
 def run_notify():
